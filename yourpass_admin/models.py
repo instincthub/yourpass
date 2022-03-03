6214ac1837a4796fcffcc12f9c8eda1e338a41a9
dicksondurosakin <dicksondurosakin@gmail.com>
2022-03-03 16:27:24 +0100

    second commit

--- yourpass_admin/models.py
+++ yourpass_admin/models.py
@@ -4,15 +4,14 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    # user = models.OneToOneField('auth.User',on_delete=models.CASCADE,default='me')
-    # title = models.CharField(max_length=200)
-    # description = models.TextField()
-    # location = models.CharField(max_length=200)
-    date = models.DateTimeField(null=True,blank=True)
-    # time = models.DateTimeField()
-    # image = models.ImageField(upload_to='%Y/%m/%d')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField(null=True,)
+    time = models.DateTimeField()
+    image = models.ImageField(upload_to='%Y/%m/%d')
     
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
     
     
